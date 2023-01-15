5dd2e1ea8967f9d0f84e483637a0ba6f3b7d7828
Abdur Rakib <rakib15-13656@diu.edu.bd>
2023-01-15 22:09:20 +0600

    search

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, SearhBlog
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', PostList.as_view(), name='bloglist'),
     path('<int:pk>/', PostDetail.as_view(), name="blogdetails"),
     path('auth-user/', include('rest_framework.urls')),
+    path('search/', SearhBlog.as_view(), name='searchblog'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from .serializer import PostSerializer
 from .api.permissions import IsOwnerOrReadOnly
 from rest_framework import permissions
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.views import APIView
+from django.db.models import Q
+from rest_framework.response import Response
 # Create your views here.
  
 
@@ -19,3 +22,15 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Blog.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class SearhBlog(APIView):
+
+    def post(self, request, format=None):
+        data = self.request.data
+        str = data['str']
+        q = (Q(title__icontains = str))
+        queryset = Blog.objects.all()
+        queryset = queryset.filter(q)
+        serializer = PostSerializer(queryset, many=True)
+        return Response (serializer.data)
