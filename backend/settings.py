1f1eebe47edc1e761f48532a1915f615ea59d59b
Abdur Rakib <rakib15-13656@diu.edu.bd>
2023-02-05 16:21:06 +0600

    dfdf

--- backend/settings.py
+++ backend/settings.py
@@ -84,17 +84,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Blog',
-        'USER' : 'postgres',
-        'PASSWORD' : 'rakib13656',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432'
-    }
-}
 
+import dj_database_url
+import os
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -129,7 +124,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-import os
 STATIC_URL = 'static/'
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
