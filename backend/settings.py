6f8550f60e777ba0b616e51ea12520b340d70432
Abdur Rakib <rakib15-13656@diu.edu.bd>
2023-04-03 12:25:11 +0600

    sds

--- backend/settings.py
+++ backend/settings.py
@@ -83,17 +83,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Blog',
-        'USER' : 'postgres',
-        'PASSWORD' : 'rakib13656',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432'
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
